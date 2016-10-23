8e2fcbc7f44b9745a9324f55871282d07b0651b0
murnat98 <murnat98@yandex.ru>
2016-10-23 16:27:45 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int main()
+{
+    char * AsmTxt = NULL;
+    char * FName = NULL;
+
+    FName = GetFName ();
+
+    int _size = ReadAsm ( FName, &AsmTxt );
+    while ( _size < 0 )
+    {
+        FName = GetFName ();
+        _size = ReadAsm ( FName, &AsmTxt );
+    }
+
+    WriteObj ( AsmTxt, _size );
+
+    free ( AsmTxt );
+    free ( FName );
+
+    return 0;
+}
